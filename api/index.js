6a4ac6a16fe6d7e157e37eca3c72358777b54c20
iddreeek <159159274+iddreeek@users.noreply.github.com>
2024-07-31 11:21:24 +0800

    added cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log('Server is Running');
